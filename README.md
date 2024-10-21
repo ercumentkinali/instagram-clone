697c2fda97d4d749f8cadec847d2cd4449cc4209
Ercüment Kınalı <ercument.kinali@ogr.dpu.edu.tr>
2024-10-21 13:59:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+#Instagram Clone
+
+This project is a design clone of Instagram's login page and homepage.
+
+# To start the project, open the 'login.html' file.
+
+!!!!You cannot access the homepage without logging in.
+
+In the username and password fields;
+ # email: john@mail.com
+ # password:changeme
+
+you can log in by entering.
+
+
+
+
+
---
+++ js/login.js
@@ -0,0 +1,46 @@
+document.getElementById('loginForm').addEventListener('submit', async function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('https://api.escuelajs.co/api/v1/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            console.log('Giriş başarılı:', data);
+            sessionStorage.setItem('isLoggedIn', 'true'); 
+            window.location.href = 'index.html';
+        } else {
+            alert('Giriş başarısız: ' + data.message);
+        }
+    } catch (error) {
+        console.error('API isteği başarısız:', error);
+    }
+});
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = (i === index) ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); 
